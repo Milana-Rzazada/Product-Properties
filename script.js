86eb0571132defc970829ff53f857d0d262cb9a2
Milana-Rzazade <71l1c0h@code.edu.az>
2025-08-13 17:12:07 -0400

    Test the function for some products

--- script.js
+++ script.js
@@ -18,3 +18,23 @@ class ProductProperties{
 }
 
 const product1 = new ProductProperties("Apple", 1.00, 5);
+const product2 = new ProductProperties("Banana", 0.50, 10);
+const product3 = new ProductProperties("Orange", 1.50, 7);
+const product4 = new ProductProperties("Grapes", 2.00, 3);
+const product5 = new ProductProperties("Mango", 3.00, 2);
+const product6 = new ProductProperties("Pineapple", 4.00, 1);
+const product7 = new ProductProperties("Watermelon", 5.00, 4);
+console.log(product1.toString());
+console.log("Total Value: $" + product1.getTotalValue());
+console.log(product2.toString());
+console.log("Total Value: $" + product2.getTotalValue());
+console.log(product3.toString());
+console.log("Total Value: $" + product3.getTotalValue());
+console.log(product4.toString());
+console.log("Total Value: $" + product4.getTotalValue());
+console.log(product5.toString());
+console.log("Total Value: $" + product5.getTotalValue());
+console.log(product6.toString());
+console.log("Total Value: $" + product6.getTotalValue());
+console.log(product7.toString());
+console.log("Total Value: $" + product7.getTotalValue());
